aeb0755298d5d781e725998bb91c608dda6675b8
Prajwalhn18 <prajwalhaniya18@gmail.com>
2022-02-04 21:46:25 +0530

    todo app

--- src/Components/ToDoList.js
+++ src/Components/ToDoList.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ToDo from './ToDo';
 
-const ToDoList = ({ toDoList }) => {
+const ToDoList = ({ toDoList, handleToggle, handleFilter }) => {
     return (
         <div>
             {toDoList.map((todo) => {
                 return <ToDo todo={todo} />;
             })}
+            <button style={{ margin: '20px' }} onClick={handleFilter}>
+                Clear Completed
+            </button>
         </div>
     );
 };
